adc7d38e83179cc2a9c39e751829228408c28a1a
Adele Noronha <adele.c.noronha@gmail.com>
2021-04-03 17:56:55 +0000

    Add to user schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,9 @@
 ---Apr 3rd - Adele will be working here--
 
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, creator_status)
+VALUES ('Alice', 'alice.cooks.lots@mail.com', 'password', FALSE), ('Bob', 'bob.writes@mail.com', 'password', TRUE),
+('Carol', 'carol@carol.com', 'password', FALSE);
+
+
+-- INSERT INTO users (name) VALUES ('Kira');
