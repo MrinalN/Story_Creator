3455008a442f2d455d7793aaa36d9c30457451d7
Adele Noronha <adele.c.noronha@gmail.com>
2021-04-03 20:17:28 +0000

    Add comments to insert schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,52 +1,52 @@
--- Drop and recreate Users table (Example)
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
-
--- ---Apr 3rd - Adele will be working here--
+-- Drop and recreate Users table (Example they gave us)
 -- DROP TABLE IF EXISTS users CASCADE;
 -- CREATE TABLE users (
 --   id SERIAL PRIMARY KEY NOT NULL,
---   name VARCHAR(255) NOT NULL,
---   email VARCHAR(225) NOT NULL,
---   password VARCHAR(255) NOT NULL,
---   sign_up_date TIMESTAMP,
-
---   creator_status BOOLEAN DEFAULT FALSE
+--   name VARCHAR(255) NOT NULL
 -- );
 
+-- -- ---Apr 3rd - Adele will be working here--
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(225) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  sign_up_date TIMESTAMP,
 
--- DROP TABLE IF EXISTS stories CASCADE;
--- CREATE TABLE stories (
---   id SERIAL PRIMARY KEY NOT NULL,
---   title VARCHAR(255) NOT NULL,
---   description TEXT NOT NULL,
-
---   start_date TIMESTAMP,
---   publish_date TIMESTAMP,
-
---   publish_status BOOLEAN DEFAULT FALSE,
-
---   creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
---   likes_id INTEGER REFERENCES likes(id) ON DELETE CASCADE
--- );
+  creator_status BOOLEAN DEFAULT FALSE
+);
 
 
--- DROP TABLE IF EXISTS likes CASCADE;
--- CREATE TABLE likes (
---   id SERIAL PRIMARY KEY NOT NULL,
---   story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
---   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
--- );
+DROP TABLE IF EXISTS stories CASCADE;
+CREATE TABLE stories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+
+  start_date TIMESTAMP,
+  publish_date TIMESTAMP,
+
+  publish_status BOOLEAN DEFAULT FALSE,
+
+  creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
 
 
--- DROP TABLE IF EXISTS contributions CASCADE;
--- CREATE TABLE contributions (
---   id SERIAL PRIMARY KEY NOT NULL,
---   story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
---   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+DROP TABLE IF EXISTS likes CASCADE;
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
 
---   content TEXT
--- );
+
+DROP TABLE IF EXISTS contributions CASCADE;
+CREATE TABLE contributions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  likes_id INTEGER REFERENCES likes(id) ON DELETE CASCADE,
+
+  content TEXT
+);
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,9 +1,36 @@
----Apr 3rd - Adele will be working here--
+-- -- Users table seeds here (Example they gave us)
+-- INSERT INTO users (name) VALUES ('Alice');
+-- INSERT INTO users (name) VALUES ('Kira');
 
--- Users table seeds here (Example)
+-- ---Apr 3rd - Adele will be working here--
 INSERT INTO users (name, email, password, creator_status)
-VALUES ('Alice', 'alice.cooks.lots@mail.com', 'password', FALSE), ('Bob', 'bob.writes@mail.com', 'password', TRUE),
+VALUES ('Alice', 'alice.cooks.lots@mail.com', 'password', FALSE),
+('Bob', 'bob.writes@mail.com', 'password', TRUE),
 ('Carol', 'carol@carol.com', 'password', FALSE);
 
+--current sample stories all be writen by Bob (users.id 2) who is the only one with TRUE creator_status
+INSERT INTO stories (title, description, publish_status, creator_id) --timestamp
+--Stories.id 1 in progress. Stories.id 2 published.
+VALUES ('The Lost Shoe', 'Once I lost my shoe when walking through the forest.', FALSE, 2),
+('Zorro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta, augue in fermentum vulputate, felis ligula auctor nunc, non rhoncus ex nisl sit amet ligula. Aliquam vehicula sem at purus luctus semper. Morbi pulvinar eu turpis at lacinia. Sed a enim pellentesque, porttitor lacus at, vulputate nisl. Etiam et ligula eget dolor tempus gravida sit amet ac risus. Sed sollicitudin facilisis consequat. Mauris ex dolor, vulputate ut velit in, luctus pharetra tortor. Fusce pulvinar, risus eget scelerisque bibendum, augue sem interdum massa, a convallis dui sem quis lacus. Aliquam erat volutpat. Mauris quis commodo lacus. Nunc turpis nisl, semper at sapien sed, viverra luctus elit. Fusce placerat pretium orci. Integer enim orci, tristique sit amet iaculis ut, eleifend eu turpis.', TRUE, 2);
 
--- INSERT INTO users (name) VALUES ('Kira');
+
+
+INSERT INTO contributions (story_id, user_id, likes_id, content) --timestamp
+-- Alice contributes to Bob's 1st story.
+VALUES (1, 1, 2, 'I thought my laces were tied tight, but I guess not!'),
+-- Carol contributes to Bob's 1st story. No likes for contribition.
+(1, 3, null, '"My mom is going to be so mad at me!" I thought to myself.'),
+-- Alice contributed to Bob's 2st story. Already published.
+(2, 1, 3, 'Mauris ex dolor, vulputate ut velit in,' ),
+-- Alice contributes again to Bob's 1st story.
+(1, 1, null, 'This always happens when I want pizza.');
+
+
+INSERT INTO likes (contribution_id, user_id)
+--Carol likes Alice's contribution to 'The Lost Shoe' story
+VALUES(3, 1),
+--Bob likes Alice's contribution
+(2, 1),
+-- Alice likes her own contribution to Bob's 'Zorro'
+(3, 1);
